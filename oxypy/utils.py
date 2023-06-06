93937d864d57436fffbb25ad80050bb1b669b1c0
Ross Morgan <morgan.ross.512@gmail.com>
2023-06-06 16:28:00 +0100

    add Panic class body

--- oxypy/utils.py
+++ oxypy/utils.py
@@ -9,8 +9,15 @@ class Null:
 
 
 class Panic(Exception):
-    ...  # noqa
+    def __init__(self, message: str) -> None:
+        self.__message = message
+        super().__init__()
 
+    def __repr__(self) -> str:
+        name = type(self).__name__
+        message = self.__message
+
+        return f"{name}({message!r})"
 
 class Default(Protocol, Generic[T_co]):
     @classmethod
