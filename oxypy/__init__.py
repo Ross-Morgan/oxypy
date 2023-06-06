f056f168dd784e76a6f24d0085b2bc5df5836574
Ross-Morgan <morgan.ross.512@gmail.com>
2023-06-06 15:25:28 +0100

    add metadata

--- oxypy/__init__.py
+++ oxypy/__init__.py
@@ -1,4 +1,7 @@
 __all__ = ["Default", "Option", "Panic", "Result"]
+__version__ = "0.1.0"
+__author__ = "Ross Morgan"
+__email__ = "rmorgan512@protonmail.ch"
 
 from .raw import Option, Result
 from .utils import Default, Panic
