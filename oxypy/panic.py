75a97d62ad47e6dbe2fcb373bea38495dd7d77ab
Ross-Morgan <rmorgan512@protonmail.ch>
2023-08-23 02:52:38 +0100

    add panic function and document Panic class

--- oxypy/panic.py
+++ oxypy/panic.py
@@ -1,4 +1,10 @@
+import sys
+import typing
+
 class Panic(Exception):
+    """
+    A very basic Exception wrapper for Rust's `panic!()` functionality
+    """
     def __init__(self, message: str) -> None:
         self.__message = message
         super().__init__()
@@ -8,3 +14,11 @@ class Panic(Exception):
         message = self.__message
 
         return f"{name}({message!r})"
+
+    def __call__(*args, **kwargs) -> typing.NoReturn:
+        sys.exit_(1)
+
+
+def panic(*, msg: str) -> typing.NoReturn:
+    print("Panicked:", msg)
+    sys.exit_(1)
