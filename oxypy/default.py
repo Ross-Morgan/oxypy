2dbb809263385e566a6bc21da5e3a8dc7ab412b9
Ross-Morgan <rmorgan512@protonmail.ch>
2023-08-23 02:52:11 +0100

    add documentation for Default

--- oxypy/default.py
+++ oxypy/default.py
@@ -4,6 +4,9 @@ T_co = TypeVar("T_co", covariant=True)
 
 
 class Default(Protocol, Generic[T_co]):
+    """
+    A protocol that gives a type to ability to have a default value
+    """
     @classmethod
     def default(cls: Type[T_co]) -> T_co:
         ...  # noqa
