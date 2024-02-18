8de2eee624f6d2cd37388683eb8adbac12e32bc3
Ross-Morgan <morgan.ross.512@gmail.com>
2024-02-18 20:23:06 +0000

    update debug function

--- oxypy/debug.py
+++ oxypy/debug.py
@@ -3,11 +3,14 @@ from typing import Protocol
 
 class Debug(Protocol):
     """
-    A protocol that gives at type a debug-friendly representation
+    A protocol that gives at type a user-friendly representation
     """
     def debug_string(self) -> str:
         ...
 
 
-def dbg(args: list[Debug]):
-    print(*map(Debug.debug_string, args))
+def dbg(*args: Debug, **kwargs: Debug):
+    formatted_tuple = (str(v) for v in args)
+    formatted_dict = (f"{k}={v.debug_string()}" for k, v in kwargs.items())
+
+    print(*formatted_tuple, *formatted_dict)
