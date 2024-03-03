5d9fc1ce1f39e038a2c072828a6a0d59f54c5d4c
Ross-Morgan <morgan.ross.512@gmail.com>
2024-03-03 23:02:57 +0000

    update members

--- oxypy/__init__.py
+++ oxypy/__init__.py
@@ -2,14 +2,19 @@
 
 """
 
-__all__ = ["Either", "Debug", "Default", "Option", "Panic", "Result"]
+__all__ = [
+    "Either", "Option", "Result",
+    "dbg", "Debug", "Default", "Panic",
+    "PartialEq", "PartialOrd",
+]
 __version__ = "1.0.1"
 __version_info__ = (1, 0, 1)
 __author__ = "Ross Morgan"
 __email__ = "rmorgan512@protonmail.ch"
 
-from .debug import Debug
+from .debug import Debug, dbg
 from .default import Default
+from .ops import PartialEq, PartialOrd
 from .panic import Panic
 
 from .either import Either
