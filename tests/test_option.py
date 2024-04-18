93ace3db88a1c1b1799e80614ab7170ea081d03e
Cryptollusion <rmorgan512@protonmail.ch>
2024-04-18 10:27:05 +0100

    type annotate tests

--- tests/test_option.py
+++ tests/test_option.py
@@ -1,9 +1,9 @@
 from oxypy import Option
 
 
-def test_is_none_and_some():
+def test_is_none_and_some() -> None:
     none_opt = Option.none()
-    some_opt = Option.some(10)
+    some_opt = Option.some(42)
 
     assert none_opt.is_none() is True
     assert none_opt.is_some() is False
@@ -12,7 +12,7 @@ def test_is_none_and_some():
     assert some_opt.is_some() is True
 
 
-def test_unwrap():
+def test_unwrap() -> None:
     some_opt = Option.some(100)
 
     assert some_opt.unwrap() == 100
--- tests/test_result.py
+++ tests/test_result.py
@@ -1,9 +1,9 @@
 from oxypy import Result
 
 
-def test_is_ok_and_err():
-    ok_res = Result.ok(10)
-    err_res = Result.err(20)
+def test_is_ok_and_err() -> None:
+    ok_res: Result[int, str] = Result.ok(42)
+    err_res: Result[int, str] = Result.err("Hello World!")
 
     assert ok_res.is_ok() is True
     assert ok_res.is_err() is False
@@ -12,9 +12,9 @@ def test_is_ok_and_err():
     assert err_res.is_err() is True
 
 
-def test_unwrap():
-    ok_res = Result.ok("Hello")
-    err_res = Result.err("World")
+def test_unwrap() -> None:
+    ok_res: Result[int, str] = Result.ok(42)
+    err_res: Result[int, str] = Result.err("Hello World")
 
-    assert ok_res.unwrap() == "Hello"
-    assert err_res.unwrap_err() == "World"
+    assert ok_res.unwrap() == 42
+    assert err_res.unwrap_err() == "Hello World!"
