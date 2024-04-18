c5c8a6fe9b875de7e7e43c2a0b3b3200a0004e04
Cryptollusion <rmorgan512@protonmail.ch>
2024-04-18 10:51:51 +0100

    make version string follow version info

--- oxypy/__init__.py
+++ oxypy/__init__.py
@@ -4,18 +4,19 @@
 
 __all__ = [
     "Either", "Option", "Result",
-    "dbg", "Debug", "Default", "Panic",
+    "dbg", "Debug", "Default",
     "PartialEq", "PartialOrd",
 ]
-__version__ = "1.0.1"
+
 __version_info__ = (1, 0, 1)
+__version__ = ".".join(__version_info__)
 __author__ = "Ross Morgan"
 __email__ = "rmorgan512@protonmail.ch"
 
 from .debug import Debug, dbg
 from .default import Default
 from .ops import PartialEq, PartialOrd
-from .panic import Panic
+from .panic import panic
 
 from .either import Either
 from .option import Option
