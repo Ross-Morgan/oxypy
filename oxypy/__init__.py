405a75222df22b56317b59034c26e602e741a24b
Cryptollusion <rmorgan512@protonmail.ch>
2024-04-18 10:58:47 +0100

    revert to version string

--- oxypy/__init__.py
+++ oxypy/__init__.py
@@ -8,8 +8,8 @@ __all__ = [
     "PartialEq", "PartialOrd",
 ]
 
-__version_info__ = (1, 0, 1)
-__version__ = ".".join(__version_info__)
+__version_info__ = (1, 1, 0)
+__version__ = "1.1.0"
 __author__ = "Ross Morgan"
 __email__ = "rmorgan512@protonmail.ch"
 
