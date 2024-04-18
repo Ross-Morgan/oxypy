953b3863f41efe99859af39471304984e9eb14d6
Cryptollusion <rmorgan512@protonmail.ch>
2024-04-18 10:26:38 +0100

    fix typo

--- oxypy/default.py
+++ oxypy/default.py
@@ -5,7 +5,7 @@ T_co = TypeVar("T_co", covariant=True)
 
 class Default(Protocol, Generic[T_co]):
     """
-    A protocol that gives a type to ability to have a default value
+    A protocol that gives a type the ability to have a default value
     """
     @classmethod
     def __default__(cls: Type[T_co]) -> T_co:
