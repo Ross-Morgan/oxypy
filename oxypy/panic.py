42499d3e78da8b2c30bdfa0c1957df8daf2b9564
Cryptollusion <rmorgan512@protonmail.ch>
2024-04-18 10:50:00 +0100

    print to stderr on panic and remove panic class

--- oxypy/panic.py
+++ oxypy/panic.py
@@ -2,24 +2,6 @@ import sys
 import typing
 
 
-class Panic(Exception):
-    """
-    A very basic Exception wrapper for Rust's `panic!()` functionality
-    """
-    def __init__(self, message: str) -> None:
-        self.__message = message
-        super().__init__()
-
-    def __repr__(self) -> str:
-        name = type(self).__name__
-        message = self.__message
-
-        return f"{name}({message!r})"
-
-    def __call__(*args, **kwargs) -> typing.NoReturn:
-        sys.exit(1)
-
-
 def panic(*, msg: str) -> typing.NoReturn:
-    print("Panicked:", msg)
+    print("Panicked:", msg, file=sys.stderr)
     sys.exit(1)
